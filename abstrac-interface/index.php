164d174919f7acc58cee802035ba1088df428f26
aprill18 <apriliadyrahman8@gmail.com>
2023-11-05 21:31:17 +0700

    orang 2 menambah file motor dan memperbarui index

--- abstrac-interface/index.php
+++ abstrac-interface/index.php
@@ -1,11 +1,19 @@
 <?php 
 
     include_once "Mobil.php";
+    include_once "Motor.php";
 
     $mobil1 = new Mobil();
 
     echo $mobil1->melaju();
     echo "<br/>";
     echo $mobil1->mengerem();
+    echo "<br/>";
+
+    $motor1 = new Motor();
+
+    echo $motor1->melaju();
+    echo "<br/>";
+    echo $motor1->mengerem();
 
 ?>
